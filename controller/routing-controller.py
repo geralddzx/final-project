d4f5444957bd1de9d7beeebdc4a617cd7db72685
Gerald Ding <geralddzx@gmail.com>
2020-05-20 17:11:07 -0400

    c

--- controller/routing-controller.py
+++ controller/routing-controller.py
@@ -1,15 +1,14 @@
 from p4utils.utils.topology import Topology
 from p4utils.utils.sswitch_API import SimpleSwitchAPI
-import save_topology
+import draw_topology
 
 class RoutingController(object):
 
     def __init__(self):
-
         self.topo = Topology(db="topology.db")
 
-    def init(self):
-        save_topology.run(self.topo)
+    def main(self):
+        draw_topology.run(self.topo)
 
 if __name__ == "__main__":
-    controller = RoutingController()
+    controller = RoutingController().main()
