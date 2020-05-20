38c2797d8efba280812fc1ee685adc85bbdab066
Gerald Ding <geralddzx@gmail.com>
2020-05-20 16:35:04 -0400

    it

--- controller/mds.py
+++ controller/mds.py
@@ -89,14 +89,14 @@ def draw(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
-    for num in range(num_iterations):
+    for num_iteration in range(num_iterations):
         for i in range(len(nodes)):
             for j in range(len(nodes)):
                 expected = paths[i][j]
                 if expected:
                     diff = (x[j] - x[i], y[j] - y[i])
                     distance = get_distance(diff)
-                    if num / num_iterations < 0.5:
+                    if num_iteration / num_iterations < 0.5:
                         delta = math.log(distance / expected)
                     else:
                         delta = -(expected / distance) + 1
